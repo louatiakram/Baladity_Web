7ee332e2c5105dc505af1864e96d727a2d92d866
hadilsediri <hadil.sediri@esprit.tn>
2024-03-23 02:06:18 +0100

    entities,controllers,forms
    creation de l'entité et le controller et le form d'équipement et avis

---
+++ src/Form/AvisType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('note_avis', IntegerType::class)
+            ->add('commentaire_avis', TextareaType::class)
+            ->add('Save', SubmitType::class)
+            ->add('Reset', ResetType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
