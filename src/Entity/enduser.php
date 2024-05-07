e6927a89ca4d03490c494b8df8d38831895a2977
Akram <akram.louati@esprit.tn>
2024-05-07 21:43:11 +0100

    Revert "fix enduser rep"
    This reverts commit f655330e6c116a1f9cbe8be1ca5e8e3d48225a78.

--- src/Entity/enduser.php
+++ src/Entity/enduser.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\EndUserRepository;
+use App\Repository\enduserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
